1d42b5405d1d3a9aade9a9d45aca97743d248c81
leung52 <katherine.leung52@gmail.com>
2024-12-04 16:58:14 -0500

    updated file code (code/0_combine_single_cell_data.py)

--- code/0_combine_single_cell_data.py
+++ code/0_combine_single_cell_data.py
@@ -1,29 +1,35 @@
-
+from functools import reduce
 import pandas as pd
 
 
-def merge_single_cell_gene_expression_data_files(file1: str, file2: str, output_file: str) -> pd.DataFrame:
+def merge_sc_data_files(files: list(str), save_to: str) -> None:
 	print('Loading dataframes...')
-	df1 = pd.read_csv(file1, sep='\t', index_col=0)
-	df2 = pd.read_csv(file2, sep='\t', index_col=0)
+	dfs = []
+	for file in files:
+		dfs.append(pd.read_csv(file, sep='\t', index_col=0))
 	print('done.')
+
 	print('Merging dataframes into one...')
-	merge_df = df1.merge(df2, how='outer', on='GENE') # merge dataframes on genes
+	df_merged = reduce(lambda left,right: pd.merge(left,right,on=['GENE'], how='outer'), dfs)
+	del dfs
 	print('done.')
+
 	print('Replacing NaN with int(0)...')
-	merge_df = merge_df.fillna(0) # convert NaN to 0
+	merge_df = merge_df.fillna(0)
 	print('done.')
+
 	print('Ensuring data type is int...')
-	merge_df = merge_df.astype(int) # ensure data type is int
+	merge_df = merge_df.astype(int)
 	print('done.')
+
 	print('Saving dataframe to tsv...')
-	merge_df.to_csv(output_file, sep='\t') # Save to tsv file
+	merge_df.to_csv(save_to, sep='\t')
 	print('done.')
-	return merge_df
 
 
 if __name__ == "__main__":
 	tmu_file_1 = 'Tmu_scRNASeq_objects/DS50_clean_DGE_3000.txt' # tab separated
 	tmu_file_2 = 'Tmu_scRNASeq_objects/DS51_clean_DGE_3000.txt'
 	output_file = 'processed_data/DS50_DS51_clean_DGE_combined_6000.tsv'
-	merge_single_cell_gene_expression_data_files(tmu_file_1, tmu_file_2, output_file)
+	merge_sc_data_files(tmu_file_1, tmu_file_2, output_file)
+
